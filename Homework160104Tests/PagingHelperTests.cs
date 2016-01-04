85ee4d0b680aedb5534558d844f50d97e6047ae2
Calvin Yang <calvin_yang@trend.com.tw>
2016-01-04 23:28:07 +0800

    add test complete

---
+++ Homework160104Tests/PagingHelperTests.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Homework160104;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NSubstitute;
+
+namespace Homework160104.Tests
+{
+    [TestClass]
+    public class PagingHelperTests
+    {
+        private List<Product> _products;
+
+        [TestInitialize]
+        public void GetTestSource()
+        {
+            _products = new List<Product>
+                {
+                    new Product {Id = 1, Cost = 1, Revenue = 0, SellPrice = 0},
+                    new Product {Id = 2, Cost = 0, Revenue = 2, SellPrice = 0},
+                    new Product {Id = 3, Cost = 0, Revenue = 0, SellPrice = 3},
+                    new Product {Id = 4, Cost = 1, Revenue = 0, SellPrice = 3},
+                    new Product {Id = 5, Cost = 1, Revenue = 2, SellPrice = 0},
+                    new Product {Id = 6, Cost = 0, Revenue = 2, SellPrice = 3},
+                    new Product {Id = 7, Cost = 1, Revenue = 2, SellPrice = 3},
+                    new Product {Id = 8, Cost = 1, Revenue = 2, SellPrice = 3},
+                    new Product {Id = 9, Cost = 1, Revenue = 2, SellPrice = 3}
+                };
+        }
+
+        [TestMethod]
+        public void GetPagingTest_column_cost_paging_3_should_1_2_3()
+        {
+            IProductSource source = Substitute.For<IProductSource>();
+            ColumnType columnType = ColumnType.Cost;
+            int paging = 3;
+            source.GetSource().ReturnsForAnyArgs(_products);
+
+            List<int> expected = new List<int> { 1, 2, 3 };
+
+            PagingHelper helper = new PagingHelper(source);
+            List<int> actual = helper.SumByPaging(columnType, paging);
+
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetPagingTest_column_revenue_paging_3_should_2_4_6()
+        {
+            IProductSource source = Substitute.For<IProductSource>();
+            ColumnType columnType = ColumnType.Revenue;
+            int paging = 3;
+            source.GetSource().ReturnsForAnyArgs(_products);
+
+            List<int> expected = new List<int> { 2, 4, 6 };
+
+            PagingHelper helper = new PagingHelper(source);
+            List<int> actual = helper.SumByPaging(columnType, paging);
+
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetPagingTest_column_sell_price_paging_3_should_3_6_9()
+        {
+            IProductSource source = Substitute.For<IProductSource>();
+            ColumnType columnType = ColumnType.SellPrice;
+            int paging = 3;
+            source.GetSource().ReturnsForAnyArgs(_products);
+
+            List<int> expected = new List<int> { 3, 6, 9 };
+
+            PagingHelper helper = new PagingHelper(source);
+            List<int> actual = helper.SumByPaging(columnType, paging);
+
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+
+        [TestMethod]
+        public void GetPagingTest_column_cost_paging_2_should_1_1_1_2_1()
+        {
+            IProductSource source = Substitute.For<IProductSource>();
+            ColumnType columnType = ColumnType.Cost;
+            int paging = 2;
+            source.GetSource().ReturnsForAnyArgs(_products);
+
+            List<int> expected = new List<int> { 1, 1, 1, 2, 1 };
+
+            PagingHelper helper = new PagingHelper(source);
+            List<int> actual = helper.SumByPaging(columnType, paging);
+
+            CollectionAssert.AreEquivalent(expected, actual);
+        }
+    }
+}
---
+++ PagingHelper.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Homework160104
+{
+    public class PagingHelper
+    {
+        private readonly IProductSource _source;
+
+        public PagingHelper(IProductSource source)
+        {
+            _source = source;
+        }
+
+        public List<int> SumByPaging(ColumnType type, int pagingCount)
+        {
+            List<Product> products = _source.GetSource();
+
+            List<PagingProductSum> productSums = Paging(products, pagingCount);
+
+            switch (type)
+            {
+                case ColumnType.Cost:
+                    return productSums.Select(s => s.Cost).ToList();
+                case ColumnType.Revenue:
+                    return productSums.Select(s => s.Revenue).ToList();
+                case ColumnType.SellPrice:
+                    return productSums.Select(s => s.SellPrice).ToList();
+                default:
+                    throw new ArgumentOutOfRangeException("type");
+            }
+        }
+
+        private List<PagingProductSum> Paging(List<Product> products, int rows)
+        {
+            List<PagingProductSum> sums = new List<PagingProductSum>();
+
+            int record = Convert.ToInt16(Math.Ceiling((double)products.Count/rows));
+
+            for (int i = 1; i < record + 1 ; i++)
+            {
+                IEnumerable<Product> enumerable = products.Skip((i - 1)*rows).Take(rows);
+
+                PagingProductSum sum = new PagingProductSum();
+
+                foreach (var product in enumerable)
+                {
+                    sum.Cost += product.Cost;
+                    sum.Revenue += product.Revenue;
+                    sum.SellPrice += product.SellPrice;
+                }
+
+                sums.Add(sum);
+            }
+
+            return sums;
+        }
+    }
+
+    public class PagingProductSum
+    {
+        public int Cost { get; set; }
+        public int Revenue { get; set; }
+        public int SellPrice { get; set; }
+    }
+
+    public enum ColumnType
+    {
+        Cost, Revenue, SellPrice
+    }
+
+    public interface IProductSource
+    {
+        List<Product> GetSource();
+    }
+
+    public class ProductSource : IProductSource
+    {
+        public List<Product> GetSource()
+        {
+            return new List<Product>();
+        }
+    }
+
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Cost { get; set; }
+        public int Revenue { get; set; }
+        public int SellPrice { get; set; }
+    }
+}
